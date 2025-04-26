82beb86e87021859bb305c86edc306ce24d0e18e
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2025-04-26 04:14:40 -0300

    chore: Added mineral identifier

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import pages.Mineralogia 
+import pages.Mineralogia
+
 
 def render_sidebar():
     st.sidebar.image("assets/GeofisicaApp.svg", use_container_width=True)
     st.sidebar.info("Welcome to GeofisicaHub! Explore the tools below:")
-    
-    
